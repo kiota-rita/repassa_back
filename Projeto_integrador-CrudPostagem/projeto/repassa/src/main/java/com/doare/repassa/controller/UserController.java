17ebc7b0c76d83f50adc8b621ae7804ab4b241aa
rafaela <rafaelamachado.camara@yahoo.com.br>
2021-06-09 11:04:21 -0300

    Implementando getById e atualizarUsuario

--- Projeto_integrador-CrudPostagem/projeto/repassa/src/main/java/com/doare/repassa/controller/UserController.java
+++ Projeto_integrador-CrudPostagem/projeto/repassa/src/main/java/com/doare/repassa/controller/UserController.java
@@ -6,13 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.doare.repassa.model.User;
 import com.doare.repassa.model.UserLogin;
+import com.doare.repassa.repository.UserRepository;
 import com.doare.repassa.service.UserService;
 
 @RestController
@@ -33,4 +37,16 @@ public class UserController {
 	public ResponseEntity<User> Post(@RequestBody User email) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(userService.CadastrarUsuario(email));
 	}
+	
+	@PutMapping("/cadastrar")
+	public ResponseEntity<User> put (@RequestBody User usuario){
+		return ResponseEntity.status(HttpStatus.OK).body(userService.atualizarUsuario(usuario));
+	}
+	
+	@Autowired
+    private UserRepository repository;
+	@GetMapping("/{id}")
+    public ResponseEntity<User> GetById(@PathVariable long id) {
+        return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+    }
 }
--- Projeto_integrador-CrudPostagem/projeto/repassa/src/main/java/com/doare/repassa/service/UserService.java
+++ Projeto_integrador-CrudPostagem/projeto/repassa/src/main/java/com/doare/repassa/service/UserService.java
@@ -26,6 +26,16 @@ public class UserService {
 
 		return repository.save(usuario);
 	}
+	
+	public User atualizarUsuario(User usuario) {
+
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+		String senhaEncoder = encoder.encode(usuario.getSenha());
+		usuario.setSenha(senhaEncoder);
+
+		return repository.save(usuario);
+	} 
 
 	public Optional<UserLogin> Logar(Optional<UserLogin> user) {
 
